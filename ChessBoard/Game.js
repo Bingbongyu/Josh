bda1da0c020a45ee8a9d05c2c4365745210ef21b
Bingbongyu <55059607+Bingbongyu@users.noreply.github.com>
2020-02-24 16:10:10 -0700

    Add files via upload

---
+++ ChessBoard/Game.js
@@ -0,0 +1,24 @@
+// var Board=[8][8]; 
+// const theBoard = document.querySelector(".Board");
+// let spot = document.getElementsByClassName("piece5");
+// console.log(spot);
+
+// let n = 1;
+// let myFunction = ()=>{
+//     console.log(n);
+//     if(n%2==1)
+//     theBoard.style.transform = "rotate(180deg)";
+//     else
+//     theBoard.style.transform = "rotate(360deg)";
+//     n++;
+// };
+
+
+// function test2(){
+//     if(theBoard.addEventListener("click",test))
+//         alert("its alive!");
+// }
+// function testing(variable){
+//     console.log(variable);
+// }
+
---
+++ ChessBoard/matrixBoard.js
@@ -0,0 +1,3 @@
+var ChessBoard =[8][8];
+ChessBoard[0][0] = document.getElementsByClassName("_00");
+ChessBoard[0][0].style.color = "yellow";
